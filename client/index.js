7b281febd8fd68f83ce2428c820c9b3fecb6f94f
yuchen.huang <yuchen.huang@wz-inc.com>
2021-03-09 17:47:58 +0800

    feat: init

--- client/index.js
+++ client/index.js
@@ -1,4 +1,4 @@
-const socket = new WebSocket('ws://127.0.0.1:8080');
+const socket = new WebSocket('ws://localhost:8080');
 
 socket.addEventListener('open', e => {
     console.log('opened', e);
--- server/app.ts
+++ server/app.ts
@@ -1,10 +1,20 @@
 import { Server } from 'ws';
 
-new Server({ port: 8080 }).on('connection', socket => {
+const server = new Server({ port: 8080 });
+
+server.on('listening', () => console.log('started'));
+server.on('error', e => console.log('error', e));
+server.on('connection', socket => {
+    
+    console.log(socket.url);
+
+    socket.on('pong', () => {
+        
+    });
+
     socket.on('message', msg => {
         console.log(msg);
         socket.send('124');
     });
-}).on('error', e => {
-    console.log('error', e);
-});
+})
+
